5ba6741e44fa6c785ea4db1b6be8c833e6060d43
Jadhav4Real <jadhav4real@gmail.com>
2023-03-10 16:26:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 function App() {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='header'>
+          <nav className="navbar">
+            <a href='/' className="logo">
+              <img src={logo} alt="logo" />
+            </a>
+            <div className='burger' onClick={handleClick}>
+              {click ? (<FaTimes size={30} style={{ color: 'ffffff' }} />) : <FaBars size={30} style={{ color: '#ffffff' }} />}
+
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <a href="/">HOME</a>
+              </li>
+              <li className="nav-item">
+                <a href="tel:+91-865-281-6746">📞Call Us</a>
+              </li>
+
+            </ul>
+          </nav>
+        </div>
       </header>
+      {/* ----------------------------------------------------------------------------------- */}
+      <>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div className='hero'>
+                <div className="card">
+                    <h2>our website is under construction</h2>
+
+                    <div class="progress-value">
+                        <div class="progress-value2"></div>
+                    </div>
+
+                    <button className="btn">
+                        <a href="tel:+91-865-281-6746">📞 Call for More</a>
+                    </button>
+                </div>
+
+            </div>
+        </>
     </div>
   );
 }
